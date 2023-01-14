462ab1c7a95a382a7d27f4fb94c955661b6f8c7b
Drake Hopkins <56398897+DrakeHopkins@users.noreply.github.com>
2023-01-14 00:01:52 -0600

    init

---
+++ src/data/info.ts
@@ -0,0 +1,172 @@
+export const info = {
+  baseUrl: "https://drakeafk.net",
+  name: "Drake Hopkins",
+  // jobDescription: "Web Developer",
+  about: `I'm a Web Developer and IT Professional currently based in the United States.
+   As a web developer, I help people & businesses reach others through the internet. 
+      I operate as a Full-Stack Developer and always trying to improve my skillset.
+   As an IT Professional, I help people & businesses quickly and efficiently resolve a wide range of technical issues in areas such as hardware/software troubleshooting, infrastructure, operations, and policies & processes. 
+      I am constantly learning/researching the latest technologies in order to stay on top of constantly evolving IT trends.`,
+
+  experience: [
+    {
+      name: "Web Developer",
+      location: "Freelance",
+      startDate: "Aug 2020",
+      endDate: "Present",
+      description: [
+        "✓ Design & build websites for individuals and small businesses",
+        "✓ Documenting, testing, deployment, maintenance",
+        "✓ Code Improvements and Refactoring ",
+      ],
+    },
+
+    {
+      name: "IT Consultant",
+      location: "Freelance",
+      startDate: "Feb 2022",
+      endDate: "Present",
+      description: [
+        "✓ Work with individuals and small businesses to evaluate IT needs relating to systems and infrastructure",
+        "✓ Diagnose system problems, inefficiencies, and weaknesses",
+        "✓ Provide advice for technology 'best practices'"
+      ],
+    },
+
+    {
+      name: "Photographer/Videographer & Content Creator",
+      location: "Freelance / Self-Employed",
+      startDate: "Dec 2016",
+      endDate: "Present",
+      description: [
+        "- Owner of a photography/videography and content creation business",
+        "- Have worked with multiple clients including 'Air-Evac Lifeteam'",
+        "- Nature and wildlife photography is published in multiple areas",
+      ],
+    },
+
+    {
+      name: "IT Technician",
+      location: "Two Nurses Trading Company",
+      startDate: "Aug 2021",
+      endDate: "Nov 2022",
+      description: [
+        "- Setup and service workstations",
+        "- Install software and hardware components",
+        "- Other tasks connected to the nature of the job",
+      ],
+    },
+
+    {
+      name: "IT Technician & Web Designer",
+      location: "Farmers Elevator",
+      startDate: "Apr 2016",
+      endDate: "Jul 2019",
+      description: [
+        "- Setup and service workstations",
+        "- Computer repairs and maintenance",
+        "- Install software and hardware components",
+        "- Network administration",
+        "- Maintain and update website UI/UX with HTML/CSS/JavaScript",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Arkansas State University",
+      location: "Jonesboro, Arkansas, United States",
+      startDate: "2021",
+      endDate: "2021",
+      description: [
+        "Various Master's Level Courses",
+        "✓ Advanced Data Analysis",
+      ],
+    },
+    {
+      name: "New York University",
+      location: "New York, New York, United States",
+      startDate: "2020",
+      endDate: "2020",
+      description: [
+        "Non-Degree Software Engineering Program",
+        "✓ Computer Programming in C++",
+        "✓ Discrete Mathematics",
+        "✓ Data Structures and Algorithms",
+        "✓ Computer Networks",
+        "✓ Operating Systems",
+      ],
+    },
+    {
+      name: "Arkansas State University",
+      location: "Jonesboro, Arkansas, United States",
+      startDate: "2014",
+      endDate: "2019",
+      description: [
+        "B.A Political Science",
+        "✓ Concepts of Programming",
+        "✓ Evolution - Biology",
+        "✓ Political Engagement",
+        "✓ Principles of Public Administration",
+        "✓ Middle Eastern Political Systems",
+        "✓ International Politics",
+        "✓ Public Opinion and Public Policy",
+      ],
+    },
+  ],
+
+  certifications: [
+    {
+      name: "Information Technology",
+      description: [
+        "✓ CompTIA A+",
+        "✓ CompTIA Network+",
+        "✓ CompTIA Security+",
+        "✓ Google IT Support",
+        "✓ Google IT Automation w/ Python",
+      ],
+    },
+    {
+      name: "Programming",
+      description: [
+        "✓ Web Development Bootcamp - The App Brewery",
+        "✓ Web Development Bootcamp - Colt Steele",
+        "✓ JavaScript Bootcamp - Jonas Schmedtmann",
+        "✓ Python Bootcamp - Jose Portilla",
+        "✓ Flutter App Development w/ Dart - The App Brewery",
+        "✓ iOS App Development w/ Swift - The App Brewery",
+      ],
+    },
+    {
+      name: "MISC",
+      description: [
+        "✓ TEFL/TESOL",
+      ],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/drakeafk",
+    linkedin: "https://www.linkedin.com/in/drakehopkins/",
+    email: "drakehopkins.dev@gmail.com",
+    twitter: "https://twitter.com/DrakeAFK_",
+    instagram: "https://instagram.com/drakeafk_",
+  },
+
+  projects: [
+    {
+      title: "Colored Memory",
+      isFeatured: true,
+      thumbnail: "/assets/images/colored-memory-screenshot.png",
+      githubUrl: "https://github.com/drakeafk/colored-memory",
+      liveUrl: "https://www.drakehopkins.com/projects/color-memory/index.html",
+    },
+    {
+      title: "vDrums",
+      isFeatured: true,
+      thumbnail: "/assets/images/vDrums-screenshot.png",
+      githubUrl: "https://github.com/drakeafk/vDrums",
+      liveUrl: "https://www.drakehopkins.com/projects/drum-set/index.html",
+    },
+  ],
+};
